0edf4965b2d2c31ae33658f1052e695313e36d2d
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-04-28 13:35:37 +0900

    uploaded-files/<ing:pk>を追加

--- spokenMaterial/urls.py
+++ spokenMaterial/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
         'get': 'list',
     }), name='uploaded-files-list'),
 
+    path('uploaded-files/<int:pk>/', UploadedFileViewSet.as_view({
+        'get': 'retrieve'
+    }), name='uploaded-file-detail'),
+
     # UploadedFileのIDに紐づいたTranscriptionの一覧を取得するための新しいパス
     path('transcriptions/uploaded-file/<int:uploadedfile_id>/', TranscriptionViewSet.as_view({
         'get': 'list'
