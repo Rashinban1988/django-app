7997c1987bf093c98c4b50d0179eae2733a8b0fc
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-04-30 19:44:18 +0900

    celery設定を追加

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import HttpResponse
+from django.core.cache import cache
+import logging
 
 def index(request):
+    cache.set('test', 'value', timeout=30)  # Redis にデータを書き込むテスト
+    print(cache.get('test'))  # 書き込んだデータを読み出すテスト\
+    logger = logging.getLogger(__name__)
+    logger.debug("ファイルアップロードがリクエストされました。")
+    logger.debug(cache.get('test'))  # 書き込んだデータを読み出すテスト
     return HttpResponse('Hello world!')
