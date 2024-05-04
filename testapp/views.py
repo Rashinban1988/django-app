ab9847a12fb6a56ee7fb292acb9c9a90bcf27fac
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-05-04 09:56:19 +0900

    ログ設定を追加

--- testapp/views.py
+++ testapp/views.py
@@ -4,8 +4,14 @@ import logging
 
 def index(request):
     cache.set('test', 'value', timeout=30)  # Redis にデータを書き込むテスト
-    print(cache.get('test'))  # 書き込んだデータを読み出すテスト\
     logger = logging.getLogger(__name__)
+    logger.debug("キャッシュに 'test' をセットしました: 'value'")
+
+    cached_value = cache.get('test')  # 書き込んだデータを読み出すテスト
+    if cached_value:
+        logger.debug(f"キャッシュから 'test' を取得しました: {cached_value}")
+    else:
+        logger.error("キャッシュから 'test' を取得できませんでした。")
+
     logger.debug("ファイルアップロードがリクエストされました。")
-    logger.debug(cache.get('test'))  # 書き込んだデータを読み出すテスト
     return HttpResponse('Hello world!')
--- xserver/settings.py
+++ xserver/settings.py
@@ -143,6 +143,25 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static') # collectstatic で集めたフ�
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
