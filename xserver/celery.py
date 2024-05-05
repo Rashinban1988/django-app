5e280451b9b2e1bf666dc3d4b0021b9fb66b554b
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-05-05 11:37:06 +0900

    Celeryの使用スレッドを2つに制限

--- xserver/celery.py
+++ xserver/celery.py
@@ -3,6 +3,7 @@ from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xserver.settings")
+os.environ['OMP_NUM_THREADS'] = '2'  # Limit the number of threads to 2
 
 app: Celery = Celery("app")
 
